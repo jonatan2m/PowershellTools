82ae2c114f34886129173d02f22be8f66a308612
= <=>
2016-08-08 16:22:02 -0300

    update dbinfo default and add more parameters to automated sqlbackup script

--- SqlAutomateBackup.ps1
+++ SqlAutomateBackup.ps1
@@ -1,12 +1,12 @@
 ﻿ Write-Host "Updating  ExecutionPolicy "
  #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 
-.  ($PSScriptRoot + '\SqlBackupRestore.ps1')
+. ($PSScriptRoot + '\SqlBackupRestore.ps1')
 
 $items = Get-Content -Raw -Path  ($PSScriptRoot + '\dbinfo.json') | ConvertFrom-Json
 	
 foreach($i in $items){
-    Write-Host "Generating backuping to " + $i.alias
-    New-SqlBackup -Instance $i.server -Login $i.username -Password $i.passw
+    Write-Host "Generating backuping to " $i.alias
+    New-SqlBackup -Instance $i.server -Login $i.username -Password $i.passw -BackupDirectory $i.path -Database $i.dbName
 }
 
